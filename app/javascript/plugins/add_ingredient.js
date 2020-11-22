fd4a586e12c08cbd453d9b8d17d0311f10f74cc4
Pierre Damien Caux <pierre-damien_caux@hotmail.fr>
2020-11-22 19:03:19 +0100

    add some css on card and insert a new dose creation

--- app/javascript/plugins/add_ingredient.js
+++ app/javascript/plugins/add_ingredient.js
@@ -1,10 +1,10 @@
 const addIngredient = () => {
   document.querySelector(".button-purple-add").addEventListener("click", (event) => {
     event.preventDefault();
-    if (document.querySelector(".card-new-dose").style.display === "") {
-      document.querySelector(".card-new-dose").style.display = 'inline-block';
+    if (document.querySelector(".dose-form").style.display === "") {
+      document.querySelector(".dose-form").style.display = 'flex';   
     } else {
-      document.querySelector(".card-new-dose").style.display = '';
+      document.querySelector(".dose-form").style.display = '';
     }
   })
 };
